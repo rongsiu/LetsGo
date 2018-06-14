08ee250d5ce3f8b1c4465ef6a031b327afd83ce2
Rongsiu Lim <rongsiu@gmail.com>
2018-06-14 03:08:28 -0700

    add database file and seeded database

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,30 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Trips from './Trips.jsx';
 import Pack from './Pack.jsx';
+import $ from 'jquery';
 
-const App = (props) => {
-  return (
-    <div >
-      <div className="row">
-        <form className="form-group col-md-6 col-sm-12">
-          <input type="text" placeholder="Search for a Trip" />
-          <input type="submit" value="Search" />
-        </form>
-        <div className="form group col-md-6 col-sm-12">
-          <input type="text" placeholder="Location" />
-          <input type="date" value="Start" />
-          <input type="date" value="End" />
-          <input type="submit" value="Add" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.search = this.search.bind(this);
+  }
+
+  search() {
+
+  }
+
+
+  render() {
+    return (
+      <div >
+        <div className="row">
+          <form className="form-group col-md-6 col-sm-12">
+            <input type="text" placeholder="Search for a Trip" />
+            <input type="submit" value="Search" onClick={search} />
+          </form>
+          <div className="form group col-md-6 col-sm-12">
+            <input type="text" placeholder="Location" />
+            <input type="date" />
+            <input type="date" />
+            <input type="submit" value="Add" />
+          </div>
         </div>
+        <h3>Upcoming Trips</h3>
+        <Trips />
+        <h3>Completed Trips</h3>
+        <Trips />
+        <Pack />
       </div>
-      <h3>Upcoming Trips</h3>
-      <Trips />
-      <h3>Completed Trips</h3>
-      <Trips />
-      <Pack />
-    </div>
-  );
-};
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
---
+++ database/seeder.js
@@ -0,0 +1,93 @@
+CREATE TABLE trips
+(
+  id SERIAL PRIMARY KEY,
+  location VARCHAR,
+  end_date DATE,
+  start_date DATE 
+);
+
+INSERT INTO trips (location, end_date, start_date) VALUES ('Canada', '2018-01-11', '2018-01-19');
+INSERT INTO trips (location, end_date, start_date) VALUES ('London', '2018-02-24', '2018-02-27');
+INSERT INTO trips (location, end_date, start_date) VALUES ('Hong Kong', '2018-03-13', '2018-03-20');
+INSERT INTO trips (location, end_date, start_date) VALUES ('New York', '2018-04-11', '2018-04-18');
+INSERT INTO trips (location, end_date, start_date) VALUES ('Yosemite', '2018-05-16', '2018-05-22');
+INSERT INTO trips (location, end_date, start_date) VALUES ('Lake Tahoe', '2018-06-23', '2018-07-03');
+INSERT INTO trips (location, end_date, start_date) VALUES ('Berlin', '2018-07-15', '2018-07-28');
+INSERT INTO trips (location, end_date, start_date) VALUES ('Vancouver', '2018-10-09', '2018-10-19');
+
+CREATE TABLE shared_items
+(
+  id SERIAL PRIMARY KEY,
+  trip_id INT NOT NULL REFERENCES trips(id),
+  item VARCHAR,
+  added_by VARCHAR,
+  claimed_by VARCHAR
+);
+
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (1, 'tent', 'mary', 'rachel');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (1,'lamp', 'kent', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (1,'water filter', 'rachel', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (1,'pot', 'sam', 'kent');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (2, 'tent', 'mary', 'rachel');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (2,'lamp', 'kent', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (2,'water filter', 'rachel', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (2,'pot', 'sam', 'kent');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (3, 'tent', 'mary', 'rachel');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (3,'lamp', 'kent', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (3,'water filter', 'rachel', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (3,'pot', 'sam', 'kent');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (4, 'tent', 'mary', 'rachel');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (4,'lamp', 'kent', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (4,'water filter', 'rachel', 'tom');
+INSERT INTO shared_items (trip_id, item, added_by, claimed_by) VALUES (4,'pot', 'sam', 'kent');
+
+
+CREATE TABLE favor_items
+(
+  id SERIAL PRIMARY KEY,
+  trip_id INT NOT NULL REFERENCES trips(id),
+  item VARCHAR,
+  added_by VARCHAR,
+  claimed_by VARCHAR
+);
+
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (1, 'hiking poles', 'mary', 'rachel');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (1,'head lamps', 'mary', 'tom');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (2, 'hiking poles', 'mary', 'rachel');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (2,'head lamps', 'mary', 'tom');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (3, 'hiking poles', 'mary', 'rachel');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (3,'head lamps', 'mary', 'tom');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (4, 'hiking poles', 'mary', 'rachel');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (4,'head lamps', 'mary', 'tom');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (5, 'hiking poles', 'mary', 'rachel');
+INSERT INTO favor_items (trip_id, item, added_by, claimed_by) VALUES (5,'head lamps', 'mary', 'tom');
+
+
+CREATE TABLE personal_items
+(
+  id SERIAL PRIMARY KEY,
+  trip_id INT NOT NULL REFERENCES trips(id),
+  item VARCHAR
+);
+
+INSERT INTO personal_items (trip_id, item) VALUES (1, 'water bottle');
+INSERT INTO personal_items (trip_id, item) VALUES (1,'sweater');
+INSERT INTO personal_items (trip_id, item) VALUES (1, 'cap');
+INSERT INTO personal_items (trip_id, item) VALUES (1,'sunscreen');
+INSERT INTO personal_items (trip_id, item) VALUES (1, 'lip balm');
+INSERT INTO personal_items (trip_id, item) VALUES (1,'hiking shoes');
+INSERT INTO personal_items (trip_id, item) VALUES (1, 'boots');
+INSERT INTO personal_items (trip_id, item) VALUES (1,'towel');
+INSERT INTO personal_items (trip_id, item) VALUES (1, 'toothbursh');
+INSERT INTO personal_items (trip_id, item) VALUES (1,'hand sanitizer');
+INSERT INTO personal_items (trip_id, item) VALUES (2, 'water bottle');
+INSERT INTO personal_items (trip_id, item) VALUES (2,'sweater');
+INSERT INTO personal_items (trip_id, item) VALUES (2, 'cap');
+INSERT INTO personal_items (trip_id, item) VALUES (2,'sunscreen');
+INSERT INTO personal_items (trip_id, item) VALUES (2, 'lip balm');
+INSERT INTO personal_items (trip_id, item) VALUES (2,'hiking shoes');
+INSERT INTO personal_items (trip_id, item) VALUES (2, 'boots');
+INSERT INTO personal_items (trip_id, item) VALUES (2,'towel');
+INSERT INTO personal_items (trip_id, item) VALUES (2, 'toothbursh');
+INSERT INTO personal_items (trip_id, item) VALUES (2,'hand sanitizer');
+
